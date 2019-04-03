f30f5905d1294871c40a0a574e2788dd482d1d21
João Vitor Scheuermann <joao.scheuermann.amadre@youse.com.br>
2019-04-03 15:26:17 -0300

    Fisrt commit - setup the project

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <div>{{ sum }}</div>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+
+  data() {
+    return {
+
+    };
+  },
+
+  mounted() {
+
+  },
+};
+</script>
+
+<style lang="scss" module>
+
+</style>
